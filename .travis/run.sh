e9628b9ea80c47d03db0351bfc96a3beb5efbda4
Roman Liubich <roman.liubich@siemens.com>
2018-11-27 11:08:30 +0100

    Add Travis

---
+++ .travis/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
+fi
+
+conan remote add rhard "https://api.bintray.com/conan/rhard/conan"
+conan install .
+cmake
+cmake --build . --target imgx_test
