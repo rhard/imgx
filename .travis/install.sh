6f87aa2446c54c1d2bc0acab25923b8c5241a0bd
rhard <rhard.x.plane@gmail.com>
2020-10-18 14:17:02 +0200

    Try to fix travis build errors

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,13 +4,5 @@ set -e
 set -x
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew update || brew update
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
-    brew unlink cmake
-    brew upgrade cmake
-    brew install gcc || brew link --overwrite gcc
-    brew install ninja
-    brew install conan
     conan user
 fi
